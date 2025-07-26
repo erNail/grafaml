23e3699928dd1b4be2f8fd87a37887ec17286e41
nagel.eric.95@googlemail.com <nagel.eric.95@googlemail.com>
2025-07-26 20:17:51 +0200

    chore: Remove devcontainer

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pip install -r requirements.txt
-yarn global add semantic-release@21.0.5
-brew install helmfile
-brew install norwoodj/tap/helm-docs
-brew install kube-linter
-helm plugin install https://github.com/helm-unittest/helm-unittest.git
-helm plugin install https://github.com/databus23/helm-diff
-echo "export PATH=$PATH:$(yarn global bin)" >>~/.bashrc
