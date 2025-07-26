3017b26e3c6d756a6e85a1c3ac4974e04c4afd8f
nagel.eric.95@googlemail.com <nagel.eric.95@googlemail.com>
2025-07-26 17:22:50 +0200

    chore: Update Renovate config

---
+++ renovate-config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  platform: "github",
+  repositories: [
+    process.env.RENOVATE_REPO
+  ],
+  extends: [
+    ":ignoreModulesAndTests",
+    ":enablePreCommit"
+  ],
+  pruneStaleBranches: false,
+  branchPrefix: "chore/",
+  branchNameStrict: true,
+  semanticCommits: "enabled",
+  semanticCommitScope: "dependencies",
+  commitMessageLowerCase: "never",
+  onboarding: true,
+  onboardingBranch: "chore/onboard-renovate",
+  onboardingCommitMessage: "🚀 Onboard Renovate",
+  rangeStrategy: "pin",
+  labels: ["renovate"],
+  username: "renovate-bot",
+  gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+  cachePrivatePackages: true,
+  packageRules: [
+    {
+      matchPackagePatterns: [
+        "*"
+      ],
+      semanticCommitType: "chore"
+    },
+    {
+      matchDepTypes: [
+        "dependencies",
+        "require"
+      ],
+      semanticCommitType: "fix"
+    },
+    {
+      matchManagers: [
+        "github-actions"
+      ],
+      semanticCommitType: "ci",
+    },
+    {
+      matchDatasources: [
+        "docker"
+      ],
+      pinDigests: false
+    },
+  ],
+  customManagers: [
+    {
+      customType: "regex",
+      datasourceTemplate: "docker",
+      managerFilePatterns: [
+        "/(^|/)Chart\\.yaml$/"
+      ],
+      matchStrings: [
+        "#\\s*renovate: image=(?<depName>.*?)\\s+appVersion:\\s*[\"']?(?<currentValue>[\\w+\\.\\-]*)"
+      ]
+    }
+  ]
+};
